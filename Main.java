be7feb117d2546e672f568d0f7b526c2c2a0f9b8
Nguyen <nguyenboy98@gmail.com>
2017-10-19 11:44:46 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vn.com.iu.student.demogame;
+
+/**
+ *
+ * @author nguye
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Screen screen = new Screen();
+        
+    }
+    
+}
